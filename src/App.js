6af1f0689e69ef22933a4d25c193c835bd67b562
fernanhick <fernanhick@gmail.com>
2021-02-17 00:40:58 +0000

    hashrouter added

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import Footer from "./components/footer"
 import HeroSection from "./components/herosection"
 import Navbar from "./components/navbar"
 import ProjectsSection from "./components/ProjectsSection/Index"
-import { BrowserRouter as Router } from "react-router-dom"
+import { BrowserRouter as Router, HashRouter } from "react-router-dom"
 
 function App() {
 	return (
 		<>
-			<Router>
-				<Navbar />
-				<HeroSection />
-				<ProjectsSection />
-				<Footer />
-			</Router>
+			<HashRouter basename="/">
+				<Router>
+					<Navbar />
+					<HeroSection />
+					<ProjectsSection />
+					<Footer />
+				</Router>
+			</HashRouter>
 		</>
 	)
 }
